a2f98802bb620a552c3a762a28dfc107952ddce0
gabroda34 <gabroda34@yahoo.com>
2020-02-22 18:02:26 +0800

    Lgin Reg

--- cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/MainActivity.java
+++ cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.cardinalskerrt.cultureapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ImageView startLogo;
@@ -19,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
         startLogo.startAnimation(fadeIn);
+    }
+
+}
+class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public AuthenticationPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    void addFragmet(Fragment fragment) {
+        fragmentList.add(fragment);
     }
 }
