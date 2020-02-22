2a4175fc0ac9ae25405c420ce95bb0eb018618da
ethan-clam <ethan-clam@users.noreply.github.com>
2020-02-22 14:22:13 +0800

    For the last time

--- cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/MainActivity.java
+++ cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.cardinalskerrt.cultureapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView startLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLogo = findViewById(R.id.startLogo);
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        startLogo.startAnimation(fadeIn);
+
     }
 }
