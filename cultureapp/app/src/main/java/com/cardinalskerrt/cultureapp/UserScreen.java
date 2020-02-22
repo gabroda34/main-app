e0a88eeea07355ddc5593014ebeec604fe5914bc
gabroda34 <gabroda34@yahoo.com>
2020-02-22 16:36:33 +0800

    Revert "Gradle Downgrade"
    This reverts commit f5f6e42edb087b926fda32b6fc271810c83cf1b0.

--- cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/UserScreen.java
+++ cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/UserScreen.java
@@ -5,55 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
+import android.view.View;
+import android.widget.TableLayout;
 
 public class UserScreen extends AppCompatActivity {
 
+    TableLayout userTabLayout;
     ViewPager userViewPager;
     PageAdapter userPageAdapter;
-    TabLayout userTablayout;
-    TabItem userNewsFeedTab;
-    TabItem userMarketTab;
-    TabItem userProfileTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_screen);
 
+        userTabLayout = findViewById(R.id.userTabLayout);
         userViewPager = findViewById(R.id.userViewPager);
-        userTablayout = findViewById(R.id.userTabLayout);
-        userNewsFeedTab = findViewById(R.id.userNewsfeedTab);
-        userMarketTab = findViewById(R.id.userMarketTab);
-        userProfileTab = findViewById(R.id.userProfileTab);
 
         userPageAdapter = new PageAdapter(getSupportFragmentManager(), 3);
         userViewPager.setAdapter(userPageAdapter);
-
-        userTablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                userViewPager.setCurrentItem(tab.getPosition());
-                userPageAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-        userViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(userTablayout));
-
     }
 
     //page adapter for this activity
