aa62c4920661ae630041b3d9854e04eaf298d327
ethan-clam <ethan-clam@users.noreply.github.com>
2020-02-22 15:00:37 +0800

    View Update

--- cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/UserScreen.java
+++ cultureapp/app/src/main/java/com/cardinalskerrt/cultureapp/UserScreen.java
@@ -1,14 +1,43 @@
 package com.cardinalskerrt.cultureapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
 
 public class UserScreen extends AppCompatActivity {
+
+    TableLayout userTabLayout;
+    ViewPager userViewPager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_screen);
     }
+
+    private static class PageAdapter extends PagerAdapter{
+
+        PageAdapter(FragmentManager fn, int numOfTabs){
+            super(fn);
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return false;
+        }
+    }
+
 }
